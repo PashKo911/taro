5a8cf99431809a5699f4c8f98242f0889d5c3f25
PashKo911 <rabota.pashok@gmail.com>
2024-09-04 18:25:41 +0300

    added smooth scroll

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,31 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js'
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js'
+
+import Lenis from '@studio-freight/lenis'
+
+function initializeLenis() {
+	const lenis = new Lenis({
+		duration: 1,
+		smooth: true,
+		infinite: false,
+		autoResize: true,
+	})
+
+	lenis.on('scroll', (e) => {})
+	function raf(time) {
+		lenis.raf(time)
+		requestAnimationFrame(raf)
+	}
+
+	requestAnimationFrame(raf)
+}
+
+function isTouchDevice() {
+	return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0
+}
+
+if (!isTouchDevice()) {
+	initializeLenis()
+}
