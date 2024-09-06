3055260edeaca4407a6e890bcb7162b01b3e1318
PashKo911 <rabota.pashok@gmail.com>
2024-09-06 12:11:58 +0300

    lenis

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,32 @@
 import { isMobile, menuInit } from './functions.js'
 // Підключення списку активних модулів
 import { flsModules } from './modules.js'
+
+import Lenis from '@studio-freight/lenis'
+
+window.onRefreshLenisScroll = () => {
+	window.lenis?.destroy()
+
+	setTimeout(() => {
+		window.lenis = new Lenis({
+			eventsTarget: document.querySelector('main'),
+			duration: 1.2,
+			infinite: false,
+			autoResize: true,
+		})
+		function raf(time) {
+			window.lenis.raf(time)
+			requestAnimationFrame(raf)
+		}
+		requestAnimationFrame(raf)
+	}, 0)
+
+	function isTouchDevice() {
+		return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0
+	}
+	if (!isTouchDevice()) {
+		window.onRefreshLinisScroll()
+	}
+}
+
+window.onRefreshLenisScroll ? window.onRefreshLenisScroll() : ''
